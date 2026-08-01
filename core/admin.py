04a486c7c5b25b38ce6e479c48d85811442a70de
nirvanajoshi <damodarj36@gmail.com>
2026-08-01 18:33:56 +0545

    Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Habit, CheckIn, Partnership, Streak
 
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(CheckIn)
+admin.site.register(Partnership)
+admin.site.register(Streak)
